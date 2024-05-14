af650d99a995e04ae7f29563f7c0c608be2c3145
J0YERIM <chosarah70@gmail.com>
2024-05-14 16:04:12 +0900

    test: wedding user 객체 생성 테스트 코드 구현

--- src/test/java/com/lovecloud/usermanagement/domain/UserTest.java
+++ src/test/java/com/lovecloud/usermanagement/domain/UserTest.java
@@ -1,6 +1,7 @@
 package com.lovecloud.usermanagement.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,5 +25,34 @@ class UserTest {
         assertEquals(UserRole.GUEST, guest.getUserRole());
         assertEquals("010-1234-5678", guest.getPhoneNumber());
         assertEquals("password", guest.getPassword());
+    }
+
+    @Test
+    @DisplayName("웨딩 유저 생성 테스트")
+    void createWeddingUserTest() {
+        WeddingUser weddingUser = WeddingUser.builder()
+                .email("wedding@example.com")
+                .name("wedding")
+                .userRole(UserRole.WEDDING_USER)
+                .phoneNumber("010-1234-5678")
+                .password("password")
+                .accountStatus(AccountStatus.ACTIVE)
+                .weddingRole(WeddingRole.BRIDE)
+                .oauthId("oauthId")
+                .oauth("oauth")
+                .invitationCode("invitationCode")
+                .build();
+
+        assertNotNull(weddingUser);
+        assertEquals("wedding@example.com", weddingUser.getEmail());
+        assertEquals("wedding", weddingUser.getName());
+        assertEquals(UserRole.WEDDING_USER, weddingUser.getUserRole());
+        assertEquals("010-1234-5678", weddingUser.getPhoneNumber());
+        assertEquals("password", weddingUser.getPassword());
+        assertEquals(AccountStatus.ACTIVE, weddingUser.getAccountStatus());
+        assertEquals(WeddingRole.BRIDE, weddingUser.getWeddingRole());
+        assertEquals("oauthId", weddingUser.getOauthId());
+        assertEquals("oauth", weddingUser.getOauth());
+        assertEquals("invitationCode", weddingUser.getInvitationCode());
     }
 }
