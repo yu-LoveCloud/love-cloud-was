04f4621ff5172b7a44f5ab3f5cc43a4a41365b00
J0YERIM <chosarah70@gmail.com>
2024-05-18 17:06:59 +0900

    feat: ProductRepository 인터페이스에 findByIdOrThrow 구현 및 ProductOptionsRepository 인터페이스 추가

--- src/main/java/com/lovecloud/productmanagement/domain/repository/ProductRepository.java
+++ src/main/java/com/lovecloud/productmanagement/domain/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.lovecloud.productmanagement.domain.repository;
 
 import com.lovecloud.productmanagement.domain.Product;
+import com.lovecloud.productmanagement.exception.NotFoundProductException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    default Product findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(NotFoundProductException::new);
+    }
 }
