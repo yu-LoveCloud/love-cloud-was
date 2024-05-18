8b80d82f9c80b968094e45b2eb51d46d2c8f76f8
J0YERIM <chosarah70@gmail.com>
2024-05-19 00:17:38 +0900

    feat: 상품 관련 도메인 Repositroy에 product를 찾는 함수 구현

--- src/main/java/com/lovecloud/productmanagement/domain/repository/ProductOptionsRepository.java
+++ src/main/java/com/lovecloud/productmanagement/domain/repository/ProductOptionsRepository.java
@@ -1,10 +1,12 @@
 package com.lovecloud.productmanagement.domain.repository;
 
 import com.lovecloud.productmanagement.domain.ProductOptions;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductOptionsRepository extends JpaRepository<ProductOptions, Long> {
 
+    List<ProductOptions> findByProductId(Long productId);
 }
