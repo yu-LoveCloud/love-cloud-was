da1ad42e9b74b804279cab2eae4dfffce5203f47
J0YERIM <chosarah70@gmail.com>
2024-05-18 17:13:07 +0900

    feat: ProductController에 상품 옵션 추가 엔드포인트 구현

--- src/main/java/com/lovecloud/productmanagement/presentation/ProductController.java
+++ src/main/java/com/lovecloud/productmanagement/presentation/ProductController.java
@@ -1,11 +1,14 @@
 package com.lovecloud.productmanagement.presentation;
 
 import com.lovecloud.productmanagement.application.ProductCreateService;
+import com.lovecloud.productmanagement.application.ProductOptionsCreateService;
+import com.lovecloud.productmanagement.presentation.request.CreateProductOptionRequest;
 import com.lovecloud.productmanagement.presentation.request.CreateProductRequest;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductCreateService productCreateService;
+    private final ProductOptionsCreateService productOptionsCreateService;
 
     @PostMapping
     public ResponseEntity<Long> creatProduct(
@@ -26,12 +30,12 @@ public class ProductController {
         return ResponseEntity.created(URI.create("/products/" + productId)).build();
     }
 
-/*    @PostMapping("/{productId}/options")
+    @PostMapping("/{productId}/options")
     public ResponseEntity<Void> addProductOptions(
             @PathVariable Long productId,
             @Valid @RequestBody CreateProductOptionRequest request
     ) {
-        productCreateService.addProductOptions(request.toCommand(productId));
-        return ResponseEntity.created(URI.create("/products/" + productId + "/options")).build();
-    }*/
+        final Long optionId = productOptionsCreateService.addProductOptions(request.toCommand(productId));
+        return ResponseEntity.created(URI.create("/products/" + productId + "/options/" + optionId)).build();
+    }
 }
