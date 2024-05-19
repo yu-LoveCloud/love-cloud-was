5acc259cb44f2b9527e59bcdb42470975b0f2022
Ahn HyungJun <ahj3579@naver.com>
2024-05-20 03:32:34 +0900

    [#37] feat: WeddingUserRepository 생성

---
+++ src/main/java/com/lovecloud/auth/exception/NotFoundUserException.java
@@ -0,0 +1,12 @@
+package com.lovecloud.auth.exception;
+
+import com.lovecloud.global.exception.ErrorCode;
+import com.lovecloud.global.exception.LoveCloudException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public class NotFoundUserException extends LoveCloudException {
+
+    public NotFoundUserException() {super(new ErrorCode(NOT_FOUND, "존재하지 않는 회원입니다."));}
+
+}
