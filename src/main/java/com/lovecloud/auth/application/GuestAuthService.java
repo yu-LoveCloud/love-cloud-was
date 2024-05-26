85ec012e1ba98e0e4eecd0d1ca389a59c8d7a323
Ahn HyungJun <ahj3579@naver.com>
2024-05-26 19:46:05 +0900

    [#54] feat: GuestSignUpRequest 생성

--- src/main/java/com/lovecloud/auth/application/GuestAuthService.java
+++ src/main/java/com/lovecloud/auth/application/GuestAuthService.java
@@ -1,9 +1,29 @@
 package com.lovecloud.auth.application;
 
+import com.lovecloud.auth.domain.GuestRepository;
+import com.lovecloud.auth.domain.GuestValidator;
+import com.lovecloud.global.crypto.CustomPasswordEncoder;
+import com.lovecloud.global.jwt.JwtTokenProvider;
+import com.lovecloud.global.jwt.refresh.RefreshTokenServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class GuestAuthService {
+
+    private final GuestRepository guestRepository;
+    private final GuestValidator validator;
+    private final CustomPasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenServiceImpl refreshTokenService;
+
+    /**
+     * GuestSignupCommand를 기반으로 회원을 생성하고, 토큰을 발급하는 메서드
+     *
+     * @param command 회원 가입에 필요한 정보를 담은  GuestSignupCommand 객체
+     * @return 토큰에 대한 JwtTokenDto 객체
+     * @throws
+     */
 }
--- src/main/java/com/lovecloud/auth/presentation/request/GuestSignUpRequest.java
+++ src/main/java/com/lovecloud/auth/presentation/request/GuestSignUpRequest.java
@@ -1,4 +1,20 @@
 package com.lovecloud.auth.presentation.request;
 
-public record GuestSignUpRequest() {
+import com.lovecloud.auth.application.command.GuestSignUpCommand;
+import jakarta.validation.constraints.NotBlank;
+
+public record GuestSignUpRequest(
+        @NotBlank String email,
+        @NotBlank String name,
+        @NotBlank String password,
+        @NotBlank String phoneNumber
+) {
+    public GuestSignUpCommand toCommand() {
+        return new GuestSignUpCommand(
+                email,
+                password,
+                name,
+                phoneNumber
+        );
+    }
 }
