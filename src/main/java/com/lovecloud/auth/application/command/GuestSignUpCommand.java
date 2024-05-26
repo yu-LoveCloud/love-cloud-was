6f9cb79b4a8306d9b2f04f182e982f43c43ff68f
Ahn HyungJun <ahj3579@naver.com>
2024-05-26 19:41:42 +0900

    [#54] feat: GuestRepository 생성

---
+++ src/main/java/com/lovecloud/auth/application/command/GuestSignUpCommand.java
@@ -0,0 +1,4 @@
+package com.lovecloud.auth.application.command;
+
+public record GuestSignUpCommand() {
+}
---
+++ src/main/java/com/lovecloud/auth/domain/GuestRepository.java
@@ -0,0 +1,19 @@
+package com.lovecloud.auth.domain;
+
+import com.lovecloud.usermanagement.domain.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lovecloud.auth.exception.NotFoundUserException;
+
+import java.util.Optional;
+
+public interface GuestRepository extends JpaRepository<Guest, Long>{
+
+    boolean existsByEmail(String email);
+
+    default Guest getByEmail(String email) {
+        return findByEmail(email)
+                .orElseThrow(NotFoundUserException::new);
+    }
+
+    Optional<Guest> findByEmail(String email);
+}
