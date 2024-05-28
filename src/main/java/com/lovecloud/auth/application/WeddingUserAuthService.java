1d521e78c85534c844bd092a75217d5005d827e8
Ahn HyungJun <ahj3579@naver.com>
2024-05-29 00:43:11 +0900

    [#70] feat: signOut 메서드 구현

--- src/main/java/com/lovecloud/auth/application/WeddingUserAuthService.java
+++ src/main/java/com/lovecloud/auth/application/WeddingUserAuthService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -68,6 +69,18 @@ public class WeddingUserAuthService {
         return jwtTokenDto;
     }
 
+    /**
+     * 로그아웃을 처리하는 메서드
+     *  - AccessToken을 Redis에 저장하여 블랙리스트 처리
+     *  - AccessToken의 만료시간까지만 저장
+     * @param token 로그아웃할 AccessToken
+     */
+    @Transactional
+    public void signOut(String token){
+        Date expirationDate = jwtTokenProvider.getExpirationDate(token);
+
+        redisTemplate.opsForValue().set(token, "blacklisted", expirationDate.getTime() - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+    }
 
 
     /**
