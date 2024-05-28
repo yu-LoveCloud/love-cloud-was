e81345d91328bf169ef3d7caa575ebe6726501a6
Yeom <ydh7219@naver.com>
2024-05-28 21:51:09 +0900

    feat: 중복된 CoupleRepository, WeddingUserRepository 제거

--- src/main/java/com/lovecloud/usermanagement/application/CoupleService.java
+++ src/main/java/com/lovecloud/usermanagement/application/CoupleService.java
@@ -2,7 +2,7 @@ package com.lovecloud.usermanagement.application;
 
 import com.lovecloud.invitationmanagement.domain.Invitation;
 import com.lovecloud.invitationmanagement.domain.repository.InvitationRepository;
-import com.lovecloud.usermanagement.repository.CoupleRepository;
+import com.lovecloud.usermanagement.domain.repository.CoupleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CoupleService {
     private final CoupleRepository coupleRepository;
     private final InvitationRepository invitationRepository;
-    public void updateCoupleInvitation(Long userId, Long invitationId){
+    public void updateCoupleInvitation(Long memberId, Long invitationId){
         Invitation invitation = invitationRepository.findByIdOrThrow(invitationId);
-        coupleRepository.findByUserIdOrThrow(userId).setInvitation(invitation);
+        coupleRepository.findByMemberIdOrThrow(memberId).setInvitation(invitation);
     }
 }
--- src/main/java/com/lovecloud/usermanagement/repository/WeddingUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lovecloud.usermanagement.repository;
-
-import com.lovecloud.usermanagement.domain.WeddingUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeddingUserRepository extends JpaRepository<WeddingUser, Long> {
-}
