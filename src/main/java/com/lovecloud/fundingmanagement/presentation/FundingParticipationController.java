2383511266b4eea59dd76b62c0462c6b211467e9
J0YERIM <chosarah70@gmail.com>
2024-05-29 10:09:25 +0900

    feat: 펀딩 참여 완료로 변경 컨트롤러 구현

--- src/main/java/com/lovecloud/fundingmanagement/presentation/FundingParticipationController.java
+++ src/main/java/com/lovecloud/fundingmanagement/presentation/FundingParticipationController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +28,14 @@ public class FundingParticipationController {
                 request.toCommand(fundingId, memberId));
         return ResponseEntity.created(
                 URI.create("/participations/" + guestFundingId)).build();
+    }
+
+    @PatchMapping("/participations/{participationId}/complete")
+    public ResponseEntity<Void> completeParticipation(
+            @PathVariable Long participationId
+    ) {
+        Long memberId = 3L; // TODO: memberId는 @Auth로 받는다고 가정
+        fundingParticipationService.completeParticipation(participationId, memberId);
+        return ResponseEntity.ok().build();
     }
 }
