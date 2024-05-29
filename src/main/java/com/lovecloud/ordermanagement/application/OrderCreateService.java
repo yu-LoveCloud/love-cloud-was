75a205c26dcc658fdc5e9e2c8644a33ffd6bd1dc
Yeom <ydh7219@naver.com>
2024-05-29 19:02:59 +0900

    feat: createOrder메소드 구현

--- src/main/java/com/lovecloud/ordermanagement/application/OrderCreateService.java
+++ src/main/java/com/lovecloud/ordermanagement/application/OrderCreateService.java
@@ -1,13 +1,64 @@
 package com.lovecloud.ordermanagement.application;
 
+import com.lovecloud.fundingmanagement.domain.Funding;
+import com.lovecloud.fundingmanagement.domain.FundingStatus;
+import com.lovecloud.fundingmanagement.domain.repository.FundingRepository;
+import com.lovecloud.ordermanagement.application.command.CreateOrderCommand;
+import com.lovecloud.ordermanagement.domain.Delivery;
+import com.lovecloud.ordermanagement.domain.DeliveryStatus;
+import com.lovecloud.ordermanagement.domain.Order;
 import com.lovecloud.ordermanagement.domain.repository.OrderRepository;
+import com.lovecloud.usermanagement.domain.Couple;
+import com.lovecloud.usermanagement.domain.repository.CoupleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class OrderCreateService {
     private final OrderRepository orderRepository;
+    private final CoupleRepository coupleRepository;
+    private final FundingRepository fundingRepository;
+    public Long createOrder(CreateOrderCommand command) {
+        Couple couple = coupleRepository.findByMemberIdOrThrow(command.userId());
+        List<Funding> fundings = fundingRepository.findAllById(command.fundingIds());
+
+        //각 펀딩에 대하여 모금이 완료된 상태인지 확인
+        fundings.forEach(funding -> {
+            if (!funding.getStatus().equals(FundingStatus.COMPLETED)) {
+                throw new IllegalArgumentException("Funding is not complete");
+            }
+            //펀딩의 주인과 주문자가 같은 커플인지 확인
+            if (!funding.getCouple().equals(couple)) {
+                throw new IllegalArgumentException("Funding owner and orderer are not the same couple");
+            }
+        });
+
+        //Delivery 생성
+        Delivery delivery = Delivery.builder()
+                .deliveryName(command.deliveryName())
+                .receiverName(command.receiverName())
+                .receiverPhoneNumber(command.receiverPhoneNumber())
+                .zipCode(command.receiverZipCode())
+                .address(command.receiverAddress())
+                .detailAddress(command.receiverAddressDetail())
+                .deliveryMemo(command.deliveryMemo())
+                .deliveryStatus(DeliveryStatus.PENDING)
+                .build();
+
+        //Order 생성
+        Order order = Order.builder()
+                .couple(couple)
+                .ordererName(command.ordererName())
+                .ordererPhoneNumber(command.ordererPhoneNumber())
+                .ordererMemo(command.ordererMemo())
+                .delivery(delivery)
+                .build();
+
+        return orderRepository.save(order).getId();
+    }
 }
