9cdd28793c2cd35afbf5fcfdf41213de5fdb4aa4
Ahn HyungJun <ahj3579@naver.com>
2024-06-05 19:59:16 +0900

    [#94] JwtAccessDeniedHandler, JwtAuthenticationEntryPoint 구현

--- src/main/java/com/lovecloud/global/jwt/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/lovecloud/global/jwt/handler/JwtAccessDeniedHandler.java
@@ -1,9 +1,8 @@
 package com.lovecloud.global.jwt.handler;
 
-import jakarta.servlet.ServletException;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
--- src/main/java/com/lovecloud/global/jwt/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/lovecloud/global/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.lovecloud.global.jwt.handler;
 
-import jakarta.servlet.ServletException;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
