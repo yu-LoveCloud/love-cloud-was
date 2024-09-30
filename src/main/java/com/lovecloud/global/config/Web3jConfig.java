b51bc0664e1be97ec39e87a7d10ad3b1bc2918b5
Ahn HyungJun <ahj3579@naver.com>
2024-09-30 15:52:54 +0900

    [#155]feat: adminCredentials Bean 등록

--- src/main/java/com/lovecloud/global/config/Web3jConfig.java
+++ src/main/java/com/lovecloud/global/config/Web3jConfig.java
@@ -2,8 +2,13 @@ package com.lovecloud.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.crypto.exception.CipherException;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
 
 @Configuration
 public class Web3jConfig {
@@ -11,5 +16,10 @@ public class Web3jConfig {
     @Bean
     public Web3j web3j() {
         return Web3j.build(new HttpService("${web3j.private-network}"));
+    }
+
+    @Bean
+    public Credentials adminCredentials() throws IOException, CipherException {
+        return WalletUtils.loadCredentials("${web3j.admin-wallet-password}", "${web3j.admin-keyfile-path}");
     }
 }
